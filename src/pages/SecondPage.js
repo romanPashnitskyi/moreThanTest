12e6d838d8bdfbb9abe27d46397c6f6d852fefeb
romanPashnitskyi <bernald_07@meta.ua>
2019-08-12 18:55:44 +0300

    added delete request

--- src/pages/SecondPage.js
+++ src/pages/SecondPage.js
@@ -17,7 +17,21 @@ class SecondPage extends Component {
       .then(res => {
         const users = res.data;
         this.setState({ users });
+
       })
+  }
+
+  deleteTodo(user) {
+    axios.delete(`http://localhost:8081/api/user/${user._id}`);
+    let todoListCopy = this.state.users; // grab a copy of the todo list
+    for (let i = 0; i < todoListCopy.length; i++) {
+      let users = todoListCopy[i];
+      if (users._id === user._id) {        // if it’s the correct ID...
+        todoListCopy.splice(i, 1)  // delete the item
+        break                      // we’re done! break the loop
+      }
+    }
+    this.setState({users: todoListCopy}) // we update state
   }
 
   render() {
@@ -26,7 +40,13 @@ class SecondPage extends Component {
         <NavigationBar back='/' forward='/third'/>
         Users:
         <ul>
-          { this.state.users.map(user => <li key={user._id}>{user.name}</li>)}
+          { this.state.users.map(user =>
+            <li key={user._id}>
+              {user.name}
+              <button type="submit" onClick={e => this.deleteTodo(user)}>
+                Delete
+              </button>
+            </li>)}
         </ul>
       </div>
     );
