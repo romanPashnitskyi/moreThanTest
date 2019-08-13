7ac98a593bfea6e1cc92d5882cdf6d00c01db4b3
romanPashnitskyi <bernald_07@meta.ua>
2019-08-13 16:56:49 +0300

    added put request

--- src/pages/SecondPage.js
+++ src/pages/SecondPage.js
@@ -8,7 +8,9 @@ class SecondPage extends Component {
     super(...args);
 
     this.state = {
-      users: []
+      users: [],
+      by: false,
+      name: ''
     };
   }
 
@@ -17,17 +19,43 @@ class SecondPage extends Component {
       .then(res => {
         const users = res.data;
         this.setState({ users });
-
       })
   }
 
-  deleteTodo(user) {
+  changeInput(field) {
+    return event => {
+      const { value } = event.target;
+
+      this.setState({ [field]: value });
+    };
+  }
+
+  edit(user, name) {
+    fetch(`http://localhost:8081/api/user`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        id: user._id,
+        name: name
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    });
+  }
+
+  handleEdit = () => {
+    this.setState({
+      by: true
+    });
+  };
+
+  delete(user) {
     axios.delete(`http://localhost:8081/api/user/${user._id}`);
     let todoListCopy = this.state.users; // grab a copy of the todo list
     for (let i = 0; i < todoListCopy.length; i++) {
       let users = todoListCopy[i];
       if (users._id === user._id) {        // if it’s the correct ID...
-        todoListCopy.splice(i, 1)  // delete the item
+        todoListCopy.splice(i, 1); // delete the item
         break                      // we’re done! break the loop
       }
     }
@@ -43,7 +71,19 @@ class SecondPage extends Component {
           { this.state.users.map(user =>
             <li key={user._id}>
               {user.name}
-              <button type="submit" onClick={e => this.deleteTodo(user)}>
+              <button type="submit" onClick={this.handleEdit}>
+                Edit
+              </button>
+              {
+                this.state.by &&
+                <form onSubmit={this.edit(user, this.state.name)}>
+                  <input type='text' required
+                         value={this.state.name}
+                         placeholder='Username'
+                         onChange={this.changeInput('name')} />
+                </form>
+              }
+              <button type="submit" onClick={e => this.delete(user)}>
                 Delete
               </button>
             </li>)}
